e224067f676c761d08daaf8945751666fba65513
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-10-09 19:59:39 +0200

    react-create-app on Heroku

--- src/reducers/moviesReducer.js
+++ src/reducers/moviesReducer.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
     switch (action.type) {
         case 'FETCH_MOVIES':
-            return [...state, ...action.payload];
+            return  action.payload;
         case 'FETCH_MOVIE_BY_QUERY':
             return action.payload;
         default:
